ab332efcb7cdbf2e31e788cac7ee84c51efd65dc
Heggel123 <80232532+Heggel123@users.noreply.github.com>
2021-08-05 11:13:47 +0700

    Add files via upload

---
+++ phpmvc/app/models/Mahasiswa_model.php
@@ -0,0 +1,85 @@
+<?php
+
+class Mahasiswa_model
+{
+
+
+    private $table = 'mahasiswa2';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function getAllMahasiswa()
+    {
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $query = "INSERT INTO mahasiswa2
+                    VALUES
+                    ('', :nama, :nrp, :email, :jurusan)";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
+    public function hapusDataMahasiswa($id)
+    {
+        $query = "DELETE FROM mahasiswa2 WHERE id = :id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
+    public function ubahDataMahasiswa($data)
+    {
+        $query = "UPDATE mahasiswa2 SET
+                nama = :nama,
+                nrp = :nrp,
+                email = :email,
+                jurusan = :jurusan
+                WHERE id = :id";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+        $this->db->bind('id', $data['id']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
+    public function cariDataMahasiswa()
+    {
+        $keyword = $_POST['keyword'];
+        $query = "SELECT * FROM mahasiswa2 WHERE nama LIKE :keyword";
+        $this->db->query($query);
+        $this->db->bind('keyword', "%$keyword%");
+        return $this->db->resultSet();
+    }
+}
---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+    private $nama = 'Ruben Heggel';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
